2752a4d1a74a825735d5f0ecc0ddb0c8f7d3e153
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-09 17:03:57 +0530

    Currency convertor

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,3 +1,3 @@
 coni = int(input('Please, enter the number of conicoins you have: '))
-rate = int(input('Please, enter the exchange rate: '))
+rate = float(input('Please, enter the exchange rate: '))
 print(f'The total amount of dollars: {coni * rate}')
