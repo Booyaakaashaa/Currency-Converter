d2fed899e037682c98f1c81505582fbb88d1fbc2
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-09 16:43:54 +0530

    Currency convertor

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,1 +1,2 @@
-print("Meet a conicoin!")
+coni = int(input())
+print(f'I have {coni} conicoins.\n{coni} conicoins cost {coni * 100} dollars.\nI am rich! Yippee!')
--- Currency Converter/task/tests.py
+++ Currency Converter/task/tests.py
@@ -5,18 +5,47 @@ from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 
 
-class TestStage1(StageTest):
+class TestStage2(StageTest):
 
     def generate(self) -> List[TestCase]:
-        return [
-            TestCase()
+        list_tests = [
+            TestCase(stdin='42', attach=['42', '4200']),
+            TestCase(stdin='75', attach=['75', '7500'])
+
         ]
 
+        return list_tests
+
     def check(self, reply: str, attach) -> CheckResult:
-        if "meet a conicoin!" not in reply.lower():
+        reply_parsed = reply.strip().split('\n')
+        if len(reply_parsed) != 3:
             return CheckResult.wrong("Check your output")
+        first_parsed = reply_parsed[0].split()
+        if len(first_parsed) != 4:
+            return CheckResult.wrong("Check your first line")
+        try:
+            amount = int(first_parsed[2])
+        except ValueError:
+            return CheckResult.wrong("Format your output"
+                                     "according to the example")
+        if amount != int(attach[0]):
+            return CheckResult.wrong("The amount of money is wrong")
+        second_parsed = reply_parsed[1].split()
+        if len(second_parsed) != 5:
+            return CheckResult.wrong("Check your second line")
+        try:
+            amount = int(second_parsed[0])
+            dollars = int(second_parsed[3])
+        except ValueError:
+            return CheckResult.wrong("Format your output"
+                                     "according to the example")
+        if amount != int(attach[0]) or dollars != int(attach[1]):
+            return CheckResult.wrong("The amount of monet is wrong")
+        if "i am rich" not in reply_parsed[2].lower():
+            return CheckResult.wrong("Your output differs from the example")
+
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    TestStage1("cconverter.cconverter").run_tests()
+    TestStage2("cconverter.cconverter").run_tests()
