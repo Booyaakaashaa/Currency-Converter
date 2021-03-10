e0c5b82d7bb4716eb544713ebcf4ac18b290e66e
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-11 01:19:40 +0530

    Currency convertor

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,3 +1,6 @@
-coni = int(input('Please, enter the number of conicoins you have: '))
-rate = float(input('Please, enter the exchange rate: '))
-print(f'The total amount of dollars: {coni * rate}')
+coni = float(input())
+print(f"""I will get {2.98 * coni} RUB from the sale of {coni} conicoins.
+I will get {0.82 * coni} ARS from the sale of {coni} conicoins.
+I will get {0.17 * coni} HNL from the sale of {coni} conicoins.
+I will get {1.9622 * coni} AUD from the sale of {coni} conicoins.
+I will get {0.208 * coni} MAD from the sale of {coni} conicoins.""")
