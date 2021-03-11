6b60e7826568df7819c5bc2865568175c367e82f
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-12 01:45:46 +0530

    Currency convertor

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,6 +1,11 @@
-coni = float(input())
-print(f"""I will get {2.98 * coni} RUB from the sale of {coni} conicoins.
+def main():
+    coni = float(input())
+    print(f"""I will get {2.98 * coni} RUB from the sale of {coni} conicoins.
 I will get {0.82 * coni} ARS from the sale of {coni} conicoins.
 I will get {0.17 * coni} HNL from the sale of {coni} conicoins.
 I will get {1.9622 * coni} AUD from the sale of {coni} conicoins.
 I will get {0.208 * coni} MAD from the sale of {coni} conicoins.""")
+
+
+if __name__ == "__main__":
+    main()
