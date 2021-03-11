1a9d99654c115a4653ff0f0af0876517de100649
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-11 22:23:38 +0530

    Currency convertor

--- Currency Converter/task/tests.py
+++ Currency Converter/task/tests.py
@@ -1,59 +1,52 @@
 from typing import List
 
+import ast
+import json
+import requests
 from hstest.check_result import CheckResult
 from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 
 
-class TestStage4(StageTest):
+class TestStage5(StageTest):
 
     def generate(self) -> List[TestCase]:
         list_tests = [
-            TestCase(stdin='17', attach=[17]),
-            TestCase(stdin='3.5', attach=[3.5]),
-            TestCase(stdin='21', attach=[21]),
-            TestCase(stdin='4.5', attach=[4.5])
+            TestCase(stdin='HNL', attach='HNL'),
+            TestCase(stdin='ILS', attach='ILS')
 
         ]
-
-        currs = {'rub': 2.98, 'ars': 0.82, 'hnl': 0.17, 'aud': 1.9622, 'mad': 0.208}
-        for test in list_tests:
-            attach_dict = {}
-            for k, v in currs.items():
-                attach_dict[k] = round(v * float(test.input), 2)
-            test.attach.append(attach_dict)
 
         return list_tests
 
     def check(self, reply: str, attach) -> CheckResult:
-        ccoins_att, currs_att = attach
-        reply_parsed = reply.strip().split('\n')
-        if len(reply_parsed) != 5:
-            return CheckResult.wrong("Your output differs from the example")
-        for repl in reply_parsed:
-            repl = repl.lower()
-            repl_parsed = repl.strip().split()
-            if len(repl_parsed) != 11:
-                return CheckResult.wrong("Your output differs from the example")
+        reply_parsed = [i for i in reply.split('\n') if i]
+        json_to_be = requests.get(f"http://www.floatrates.com/daily/{attach}.json").text
+        usd_json = json.loads(json_to_be)['usd']
+        eur_json = json.loads(json_to_be)['eur']
+        he = {}
+        jsons = [usd_json, eur_json]
+        if len(reply_parsed) != 2:
+            return CheckResult.wrong("Your output is incorrect")
+        for i, repl in enumerate(reply_parsed):
             try:
-                cur = float(repl_parsed[3])
-                ccoins = float(repl_parsed[-2])
-                key = repl_parsed[4]
-            except (ValueError, KeyError):
-                return CheckResult.wrong("Format your output according to the example")
-            if ccoins != ccoins_att:
-                return CheckResult.wrong("The amount of conicoins is wrong")
-            try:
-                amount_curr = currs_att[key]
-            except KeyError:
-                return CheckResult.wrong("The currency name in the output of your program seems to be wrong:\n"
-                                         "\"{0}\"".format(key.upper()))
-            if abs(amount_curr - cur) > 0.2:
-                return CheckResult.wrong(f"The amount of {key.upper()} is wrong")
-            if not ('i will get' in repl and 'from the sale of' in repl and 'conicoins' in repl):
-                return CheckResult.wrong("Format your output according to the example")
+                repl = ast.literal_eval(repl)
+            except SyntaxError:
+                return CheckResult.wrong("The format of the data is incorrect")
+            except ValueError:
+                return CheckResult.wrong("The format of the data is incorrect")
+            if type(repl) != dict:
+                return CheckResult.wrong("Your output should contain a dictionary.\n"
+                                         "Make sure the format of your output is correct.")
+            for key in repl.keys():
+                try:
+                    if repl[key] != jsons[i][key]:
+                        return CheckResult.wrong("Make sure your output is right")
+                except KeyError:
+                    return CheckResult.wrong("The key needed is absent in your data")
+
         return CheckResult.correct()
 
 
 if __name__ == '__main__':
-    TestStage4("cconverter.cconverter").run_tests()
+    TestStage5("cconverter.cconverter").run_tests()
