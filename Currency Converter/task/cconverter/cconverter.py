9cd47ce5ca692788f00f9043b449ddfbea7ae4df
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-13 02:16:45 +0530

    Currency convertor - json and http requests

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -5,6 +5,17 @@ import requests
 def main():
     cache = {}
     currency_code = input().lower()
+    url = f"http://www.floatrates.com/daily/{currency_code}.json"
+    data = requests.get(url)
+    json_data = json.loads(data.text)
+    if currency_code == 'usd':
+        cache['usd'] = {'rate': 1}
+    else:
+        cache["usd"] = json_data["usd"]
+    if currency_code == 'eur':
+        cache['eur'] = {'rate': 1}
+    else:
+        cache["eur"] = json_data["eur"]
     while 1:
         out_currency = input().lower()
 
@@ -19,9 +30,9 @@ def main():
                 print(f"You received {amount * cache[out_currency]['rate']} {out_currency}")
         except KeyError:
             print("Sorry, but it is not in the cache!")
+            cache[out_currency] = json_data[out_currency]
+            print(f"You received {amount * cache[out_currency]['rate']} {out_currency}.")
 
-        url = f"http://www.floatrates.com/daily/{currency_code}.json"
-        data = requests.get(url)
-        json_data = json.loads(data.text)
-        cache[out_currency] = json_data[out_currency]
-        print(f"You received {amount * cache[out_currency]['rate']} {out_currency}.")
+
+if __name__ == "__main__":
+    main()
