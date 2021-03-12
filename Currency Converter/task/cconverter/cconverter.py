2d63b43c42b81da872a677df212dc5e8bcb28e19
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-13 01:10:34 +0530

    Currency convertor - json and http requests

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,6 +1,9 @@
-import json, requests
+import json
+import requests
+
 currency_code = input().lower()
 url = f"http://www.floatrates.com/daily/{currency_code}.json"
 data = requests.get(url)
 json_data = json.loads(data.text)
+
 print(json_data["usd"], json_data["eur"], sep="\n")
