382865244782185dc3e9501697c29aac0cd8f8bb
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-13 01:39:38 +0530

    Currency convertor - json and http requests

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,9 +1,27 @@
 import json
 import requests
 
-currency_code = input().lower()
-url = f"http://www.floatrates.com/daily/{currency_code}.json"
-data = requests.get(url)
-json_data = json.loads(data.text)
 
-print(json_data["usd"], json_data["eur"], sep="\n")
+def main():
+    cache = {}
+    currency_code = input().lower()
+    while 1:
+        out_currency = input().lower()
+
+        if not out_currency:
+            break
+        amount = float(input())
+
+        print("Checking the cache...")
+        try:
+            if cache[out_currency]:
+                print("Oh! It is in the cache!")
+                print(f"You received {amount * cache[out_currency]['rate']} {out_currency}")
+        except KeyError:
+            print("Sorry, but it is not in the cache!")
+
+        url = f"http://www.floatrates.com/daily/{currency_code}.json"
+        data = requests.get(url)
+        json_data = json.loads(data.text)
+        cache[out_currency] = json_data[out_currency]
+        print(f"You received {amount * cache[out_currency]['rate']} {out_currency}.")
